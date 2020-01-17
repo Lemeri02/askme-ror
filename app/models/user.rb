01a030831ca65b55fb71767e97cab6334e94e433
Iramal <9588504@gmail.com>
2020-01-17 21:40:17 +0500

    add feature for validation email, username, password, feature for encryption password & authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,59 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  ITERATIONS = 20_000
+  DIGEST = OpenSSL::Digest::SHA256.new
+  USERNAME_PATTERN = /\A\w+\z/.freeze
+
+  attr_accessor :password
+
+  has_many :questions
+
+  validates :email, :username, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :username, length: { maximum: 40 }, format: { with: USERNAME_PATTERN }
+  validates :password, presence: true, confirmation: true, on: :create
+
+  before_validation :email_downcase
+  before_validation :username_downcase
+  before_save :encrypt_password
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? &&
+       user.password_hash == User.hash_to_string(
+         OpenSSL::PKCS5.pbkdf2_hmac(
+           password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+         )
+       )
+      user
+    else
+      nil
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def email_downcase
+    self.email&.downcase!
+  end
+
+  def username_downcase
+    self.username&.downcase!
+  end
 end
