040f2fc78b722adf903fb50d1335b9be60f880a8
Iramal <9588504@gmail.com>
2020-01-19 15:04:50 +0500

    add users&questions views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'mishka'
+      )
+    ]
   end
 
   def new
@@ -9,6 +23,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @hello = "Привет, Мир!"
+    @user = User.new(
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+      )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('19.01.2020')),
+      Question.new(text: 'How do you do?', created_at: Date.parse('21.01.2020'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
+  root 'users#index'
+
+  resources :users
+  resources :questions
   get 'show' => 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
