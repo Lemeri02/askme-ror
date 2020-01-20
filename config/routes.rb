01f3050182d25d6a4c4fcb26396556c337397e00
Iramal <9588504@gmail.com>
2020-01-20 13:10:35 +0500

    add authentication & authorization, add question controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   root 'users#index'
 
-  resources :users
-  resources :questions
-  get 'show' => 'users#show'
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :questions, except: [:show, :new, :index]
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
